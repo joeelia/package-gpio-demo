8cc5baa606d9dd91ec23e7fc0aec30cb4923b0d7
Joe Elia <44442621+joeelia@users.noreply.github.com>
2018-12-04 16:20:09 -0500

    Update node.lua

--- node.lua
+++ node.lua
@@ -10,5 +10,5 @@ util.data_mapper{
 
 function node.render()
     gl.clear(0,0,0,1)
-    font:write(250, 300, "hi", 64, 1,1,1,1) -- use the CONFIG value
+    font:write(250, 300, counter, 64, 1,1,1,1) -- use the CONFIG value
 end
