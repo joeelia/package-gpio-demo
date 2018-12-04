85ca4f331cbe380c28c3a171629bcb5f19311a64
Joe Elia <44442621+joeelia@users.noreply.github.com>
2018-12-04 16:24:07 -0500

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,14 +1,13 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font "font.ttf"
-local counter
 
 util.data_mapper{
-    counter = function(counter)
+    local counter = function(counter)
     end,
 }
 
 function node.render()
     gl.clear(0,0,0,1)
-    font:write(250, 300, counter, 64, 1,1,1,1) -- use the CONFIG value
+    font:write(250, 300, tostring(counter), 64, 1,1,1,1) -- use the CONFIG value
 end
