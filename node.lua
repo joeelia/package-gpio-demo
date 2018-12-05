67aa75b63f5d5a97baf8016887fa69e2e2c19ae5
Joe Elia <44442621+joeelia@users.noreply.github.com>
2018-12-05 11:22:55 -0500

    Update node.lua

--- node.lua
+++ node.lua
@@ -5,12 +5,17 @@ local count
 
 util.data_mapper{
     counter = function(counter)
-        count = tostring(counter)
+        counts = counter == '2'
     end,
 }
 
 function node.render()
     gl.clear(0,0,0,1)
     font:write(30, 10, "Bitcoin", 100, .5,.5,.5,1)
-    font:write(250, 300, count, 64, 1,1,1,1) -- use the CONFIG value
+    font:write(250, 300, 'count', 64, 1,1,1,1) -- use the CONFIG value
+    if counts then
+        gl.clear(0, 1, 0, 1) -- green
+    else
+        gl.clear(1, 0, 0, 1) -- red
+    end
 end
