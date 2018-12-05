7e8a5ab263bd27b625af4091d11727c26f9e8a43
Joe Elia <44442621+joeelia@users.noreply.github.com>
2018-12-05 11:10:28 -0500

    Update node.lua

--- node.lua
+++ node.lua
@@ -5,11 +5,11 @@ local count
 
 util.data_mapper{
     counter = function(counter)
-        count = json.decode(counter)
+        count = counter
     end,
 }
 
 function node.render()
     gl.clear(0,0,0,1)
-    font:write(250, 300, tostring(count), 64, 1,1,1,1) -- use the CONFIG value
+    font:write(250, 300, count, 64, 1,1,1,1) -- use the CONFIG value
 end
