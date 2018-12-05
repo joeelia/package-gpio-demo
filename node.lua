fc915656ec1818e7f5b8168a1db43e9acbbc8aa2
Joe Elia <44442621+joeelia@users.noreply.github.com>
2018-12-05 11:37:46 -0500

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,7 +1,7 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font "font.ttf"
-local count
+local count = 0
 
 util.data_mapper{
     counter = function(counter)
@@ -12,5 +12,6 @@ util.data_mapper{
 function node.render()
     gl.clear(0,0,0,1)
     font:write(30, 10, "Bitcoin", 100, .5,.5,.5,1)
-    font:write(250, 300, count, 64, 1,1,1,1) -- use the CONFIG value
+    countStr = tostring(count)
+    font:write(250, 300, countStr, 64, 1,1,1,1) -- use the CONFIG value
 end
